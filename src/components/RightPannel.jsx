9ca228af58a1b2a40199ec1e0835d313737076a8
carr-o-t <pandeystakshi@gmail.com>
2022-11-27 21:16:38 +0530

    Feat: added remove annotation

--- src/components/RightPannel.jsx
+++ src/components/RightPannel.jsx
@@ -6,6 +6,10 @@ import Icon from "../assets/icon.png";
 export function RightPannel() {
   const { records, activeIndex } = useDataContext()
   const [showLeft, setShowLeft] = React.useState(true);
+  const [isRemove, setIsRemove] = React.useState(false);
+  const [text, setText] = React.useState("");
+  const [category, setCategory] = React.useState("");
+
   const handleToggle = () => {
     setShowLeft((prev) => !prev);
     var element = document.getElementById("icon");
@@ -26,25 +30,123 @@ export function RightPannel() {
             <span>Annotations</span>
             <img src={Icon} alt="" onClick={handleToggle} />
           </span>
-          <ul>
+          <ul style={{padding: '0.8em', margin: '0'}}>
             {records[activeIndex]["person"]?.map((item) => {
               return (
-                <li className={rightpannel["list-item"]}>
-                  <span>{item}</span>
-                  <span>person</span>
+                <li
+                  className={rightpannel["list-items"]}
+                  onClick={(e) => {
+                    console.log(item, "person");
+                  }}
+                >
+                  <div className={rightpannel["list-item-text"]}>
+                    <span>{item}</span>
+                    <span>person</span>
+                  </div>
+                  <button
+                    className={rightpannel["clear-btn"]}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setIsRemove(!isRemove);
+                      setCategory("person");
+                      setText(item);
+                    }}
+                  >
+                    &times;
+                  </button>
                 </li>
               );
             })}
             {records[activeIndex]["org"]?.map((item) => {
               return (
-                <li className={rightpannel["list-item"]}>
-                  <span>{item}</span>
-                  <span>org</span>
+                <li className={rightpannel["list-items"]}>
+                  <div className={rightpannel["list-item-text"]}>
+                    <span>{item}</span>
+                    <span>org</span>
+                  </div>
+                  <button
+                    className={rightpannel["clear-btn"]}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setIsRemove(!isRemove);
+                      setCategory("org");
+                      setText(item);
+                    }}
+                  >
+                    &times;
+                  </button>
                 </li>
               );
             })}
           </ul>
+          {
+            isRemove &&
+            <RemovePopUp
+              text={text}
+              category={category}
+              onClose={(e) => setIsRemove(false)}
+            />
+          }
         </div>
       </>
     );
+}
+
+function RemovePopUp({ text, category, onClose }) {
+  const { removeAnnotation } = useDataContext();
+  const modalRef = React.useRef();
+
+  const handleRemove = () => {
+    removeAnnotation(text, category);
+    modalRef.current.click();
+  }
+
+  return (
+    <>
+      <div className={rightpannel.overlay}></div>
+      <div tabIndex={0} className={rightpannel.modal} onBlur={onClose}>
+        <div className={rightpannel["modal-header"]}>
+          <div>
+            Are you sure you want to remove the word{" "}
+            <span
+              style={{
+                background: "rgb(218, 218, 28)",
+                color: "#7a06a4",
+                fontWeight: 'bolder',
+                textTransform: 'uppercase',
+                padding: '0.1em',
+                textAlign: 'center'
+              }}
+            >
+              {text}
+            </span>{" "}
+            of{" "}
+            <span
+              style={{
+                background: "#9625be",
+                color: 'white',
+                fontWeight: 'bolder',
+                textTransform: 'uppercase',
+                padding: '0.1em',
+                textAlign: 'center'
+              }}
+            >
+              {category}
+            </span>{" "}
+            category?
+          </div>
+        </div>
+        <div className={rightpannel["modal-footer"]}>
+          <button
+            className={rightpannel["modal-cancel-btn"]}
+            ref={modalRef}
+            onClick={onClose}
+          >
+            cancel
+          </button>
+          <button className={rightpannel["modal-remove-btn"]} onClick={handleRemove}>remove</button>
+        </div>
+      </div>
+    </>
+  );
 }
